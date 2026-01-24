8b3c4effe86a1bd275fd8fdab0746428f4e99dab
lKylelUndo <ando.kyle44@gmail.com>
2026-01-24 14:56:58 +0800

    ttttt

--- app/(guest)/(index)/page.tsx
+++ app/(guest)/(index)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Home Pages</h1>
       <Button>Button Here</Button>
     </div>
   )
